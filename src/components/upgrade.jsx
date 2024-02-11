b881ca3a8fe595b82fc414c511bb4347aa5176e7
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-02-11 19:33:17 +0300

    styling adjustments

--- src/components/upgrade.jsx
+++ src/components/upgrade.jsx
@@ -5,21 +5,21 @@ const Upgrade = () => {
   return (
     <div className="relative">
       {/* Bottom Image */}
-      <img src="/Birble Dash Images/explosion.jpg" alt="Bottom Image"  style={{width: '170px', zIndex: '1', height: '110px' }} className="rounded-lg" />
+      <img src="/Birble Dash Images/explosion.jpg" alt="Bottom Image"  style={{width: '170px', zIndex: '1', height: '110px' }} className="rounded-xl" />
       <div>
         
         {/* Top-left Protruding Image */}
         <img src="/Birble Dash Images/austronaut.png" alt="Top Left Image" className="absolute top-0 left-0 -mt-10 -ml-5" style={{ width: '100px', zIndex: '3', transform: 'rotate(-15deg)' }} />
-        <div className="absolute inset-0 flex mt-12 ml-1 rounded-lg bg-black bg-opacity-70 backdrop-blur-lg place-content-evenly" style={{ zIndex: '2',  width: '160px' ,  height: '55px'}}>
+        <div className="absolute inset-0 flex mt-12 ml-1 rounded-xl bg-black bg-opacity-80 backdrop-blur-md place-content-center " style={{ zIndex: '2',  width: '160px' ,  height: '55px'}}>
             {/* Lightning Icon */}
-            <img  src="/Birble Dash Images/arrow.png"className="h-10 w-10 text-white mt-1" />
+            <img  src="/Birble Dash Images/arrow.png"className="h-10 w-10 text-white mt-2 ml-3" />
 
             {/* Text "Upgrade" */}
-            <div className="flex flex-col my-auto">
+            <div className="flex flex-col mr-3 ml-2">
                 <div className=" font-semibold text-white text-xs mt-2">Upgrade</div>
 
                 {/* Image and Text "300" */}
-                <div className="flex ">
+                <div className="flex items-center mb-1">
                     <img src="/Birble Dash Images/database.png" alt="Image" className="h-8 w-8 rounded-full " />
                     <span className=" text-lg font-semibold text-white">300</span>
                 </div>
